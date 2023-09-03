2edc434a4b608c2b9e2c6e1e396f3f6041827c24
moto <855818+mthrok@users.noreply.github.com>
2023-09-02 21:20:39 -0400

    auto-generating sphinx docs

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.0.dev20230902',
+    VERSION: '2.2.0.dev20230905',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
